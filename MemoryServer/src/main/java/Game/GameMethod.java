86c2ee7458da1d45ca07be4e2dedc6ca9a9b1efc
Pieter-Jan Robrecht <pieterjan.robrecht@student.kuleuven.be>
2016-11-14 16:10:23 +0100

    Klein foutje in verlaten spel weggewerkt

--- MemoryServer/src/main/java/Game/GameMethod.java
+++ MemoryServer/src/main/java/Game/GameMethod.java
@@ -40,6 +40,7 @@ public class GameMethod extends UnicastRemoteObject implements IGameMethod {
             }
         }
         if(index!=-1) {
+            runningGames.get(index).removeUser(user);
             if (runningGames.get(index).getAantalSpelers() == 0) {
                 runningGames.remove(index);
             }
